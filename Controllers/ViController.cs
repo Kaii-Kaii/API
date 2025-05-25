868c9c321a65c930f1308b280e261468da8df89d
keve23z <nguyenvuonghongdao2004@gmail.com>
2025-05-25 12:57:21 +0700

    Mô tả thay đổi

---
+++ Controllers/ViController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace QL_ThuChi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ViController : ControllerBase
+    {
+    }
+}
