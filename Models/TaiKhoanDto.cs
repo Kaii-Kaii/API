ce11f4ad897f6a09119e15f3eab8f341de0a6804
keve23z <nguyenvuonghongdao2004@gmail.com>
2025-06-03 00:40:46 +0700

    t muốn ngủ

---
+++ Models/TaiKhoanDto.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace QL_ThuChi.Models
+{
+    public class TaiKhoanDto
+    {
+        [Key]
+        public string MATAIKHOAN { get; set; }
+
+        public string? KhachHang { get; set; }
+        public string? MAQUYEN { get; set; }
+
+        [ForeignKey("MAQUYEN")]
+        public QuyenTruyCap? QuyenTruyCap { get; set; }
+
+        public string? TENDANGNHAP { get; set; }
+        public string? MATKHAU { get; set; }
+        public string? EMAIL { get; set; }
+        public int? ISEMAILCONFIRMED { get; set; }
+        public string? EMAILCONFIRMATIONTOKEN { get; set; }
+        public int? OTP { get; set; }
+
+        
+    }
+}
